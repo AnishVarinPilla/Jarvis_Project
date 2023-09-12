e0fb3d5d64fee239e23a222cb786b71a05f9edac
AnishVarinPilla <anishvarinpilla@gmail.com>
2023-09-12 09:40:42 +0530

    Added Dict, Alarm and OS Functionality

--- Jarvis.py
+++ Jarvis.py
@@ -1,11 +1,17 @@
 import os
 import webbrowser
+import time
 
+import keyboard
 import pyautogui
+import pyjokes
 import pyttsx3
 import pywhatkit
 import speech_recognition as sr
 import wikipedia
+from PyDictionary import PyDictionary as Diction
+import datetime
+from playsound import playsound
 
 Assistant = pyttsx3.init('sapi5') #creating a variable to take in the Microsoft Voice Lib assigned with the id = sapi5
 
@@ -106,6 +112,111 @@ def TaskExe():
             min = int(takecommand())
             pywhatkit.sendwhatmsg(ph,msg,hour,min,10)
             Speak("Ok sir sending WhatsApp message")
+    
+    def OpenApps():
+
+        Speak("Ok Sir, Wait a Second")
+        
+        if 'code' in query:
+            os.system("E:\\Microsoft VS Code\\Code.exe")
+
+        elif 'discord' in query:
+            os.system("C:\\Users\\ACER\\AppData\\Local\\Discord\\Update.exe --processStart Discord.exe")
+
+        Speak("Your Command has been executed")
+
+    def CloseApps():
+        Speak("Ok sir, wait a second")
+
+        if 'code' in query :
+            os.system("TASKKILL / F/ im Code.exe")
+        if 'discord' in query :
+            os.system ("TASKKIL/ F/ im Update.exe --processStart Discord.exe")
+        
+        Speak("Your Command has been executed")
+
+    def YoutubeAutomation():
+
+        Speak("What's your command ?")
+        comm = takecommand()
+
+        if 'pause' in comm:
+            keyboard.press('space bar')
+
+        elif 'restart' in comm:
+            keyboard.press('0')
+        
+        elif 'mute' in comm:
+            keyboard.press('m')
+
+        elif 'skip' in comm:
+            keyboard.press('l')
+
+        elif 'back' in comm:
+            keyboard.press('j')
+        
+        elif 'full screen' in comm:
+            keyboard.press('f')
+
+        elif 'film mode' in comm:
+            keyboard.press('t')
+        
+        Speak ("Done Sir")
+
+    def ChromeAutomation():
+        Speak("Chrome Automate Mode On")
+
+        command = takecommand()
+
+        if 'close this tab' in command:
+            keyboard.press_and_release ('ctrl + w')
+
+        elif 'open new tab' in command :
+            keyboard.press_and_release ('ctrl + t')
+
+        elif 'open new window' in command :
+            keyboard.press_and_release ('ctrl + n')
+        
+        elif 'history' in command:
+            keyboard.press_and_release('ctrl + h')
+        
+        elif 'download' in command :
+            keyboard.press_and_release('ctrl + j')
+
+    def screenshot():
+        Speak("What should I name it.")
+        path = takecommand()
+        path1name = path + ".png"
+        path1 = "C:\\Users\\ACER\\OneDrive\\Pictures\\Screenshots\\" +path1name #creating a path link to save the screenshot
+        ss = pyautogui.screenshot()
+        ss.save(path1)
+        os.startfile("C:\\Users\\ACER\\OneDrive\\Pictures\\Screenshots")
+        Speak("Here is your screenshot")
+
+    def Dict():
+        Speak("Tell me the word that you want me to define")
+        prob = takecommand()
+
+        if 'meaning' in prob:
+            prob = prob.replace('what is the meaning of ', '')
+            prob = prob.replace('jarvis', '')
+
+            result = Diction.meaning(prob)
+            Speak(f"The meaning for {prob} is {result}")
+
+        elif 'synonym' in prob:
+            prob = prob.replace('what is the synonym of ', '')
+            prob = prob.replace('jarvis', '')
+
+            result = Diction.synonym(prob)
+            Speak(f"The meaning for {prob} is {result}")
+        
+        elif 'antonym' in prob:
+            prob = prob.replace('what is the antonym of ', '')
+            prob = prob.replace('jarvis', '')
+
+            result = Diction.antonym(prob)
+            Speak(f"The meaning for {prob} is {result}")
 
     while True:
 
@@ -169,15 +280,99 @@ def TaskExe():
             Whatsapp()
             
         elif 'screenshot' in query:
-            Speak("What should I name it.")
-            path = takecommand()
-            path1name = path + ".png"
-            path1 = "C:\\Users\\ACER\\OneDrive\\Pictures\\Screenshots\\" +path1name
-            ss = pyautogui.screenshot()
-            ss.save(path1)
-            os.startfile("C:\\Users\\ACER\\OneDrive\\Pictures\\Screenshots")
+            screenshot()
 
+        elif 'open discord' in query:
+            query = query.replace("open ","")
+            OpenApps()
 
+        elif 'open code' in query:
+            query = query.replace("open ","")
+            OpenApps()
+
+        elif 'close discord' in query:
+            query = query.replace("close ","")
+            CloseApps()
+
+        elif 'close code' in query:
+            query = query.replace("close ","")
+            CloseApps()
+
+        elif 'pause' in query:
+            keyboard.press('space bar')
+
+        elif 'restart' in query:
+            keyboard.press('0')
+        
+        elif 'mute' in query:
+            keyboard.press('m')
+
+        elif 'skip' in query:
+            keyboard.press('l')
+
+        elif 'back' in query:
+            keyboard.press('j')
+        
+        elif 'full screen' in query:
+            keyboard.press('f')
+
+        elif 'film mode' in query:
+            keyboard.press('t')
+
+        elif 'youtube tool' in query:
+            YoutubeAutomation()
+
+        elif 'close this tab' in query:
+            keyboard.press_and_release ('ctrl + w')
+
+        elif 'open new tab' in query :
+            keyboard.press_and_release ('ctrl + t')
+
+        elif 'open new window' in query :
+            keyboard.press_and_release ('ctrl + n')
+        
+        elif 'history' in query:
+            keyboard.press_and_release('ctrl + h')
+        
+        elif 'download' in query :
+            keyboard.press_and_release('ctrl + j')
+
+        elif 'chrome automation' in query:
+            ChromeAutomation()
+        
+        elif 'joke' in query :
+            get = pyjokes.get_joke()
+            Speak(get)
+        
+        elif 'repeat my words' in query:
+            Speak("Speak Sir!")
+            jj = takecommand()
+            Speak (f"You Said : {jj}")
+        
+        elif 'my location' in query:
+            Speak ("Ok Sir, Wait A Second")
+            webbrowser.open('https://www.google.com/maps/@17.7324576,83.3041052,15z?entry=ttu')
+            Speak("Done Sir!")
+        
+        elif 'dictionary' in query:
+            Dict()
+
+        elif 'alarm' in query:
+            Speak("Enter the Time")
+            time = input(": Enter the Time :")
+
+            while True:
+                Time_AT = datetime.datetime.now()
+                now = Time_AT.strftime("%H:%M:%S")
+
+                if now == time:
+                    Speak("Time to wake up Gamer")
+                    for i in range(6):
+                        playsound('Metal_Pipe.mp3')
+                        time.sleep(1)
+                    
+                    Speak("Alarm closed ha ha")
+                    
 
 
 TaskExe()
